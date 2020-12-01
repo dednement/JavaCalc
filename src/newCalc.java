a485e3608168f408ad0657ad15c080ceadd66768
dednement <dimaon90@gmail.com>
2020-12-01 22:18:37 +0200

    first commit

---
+++ src/newCalc.java
@@ -0,0 +1,27 @@
+public class newCalc {
+        double sum(double a, double b) {
+            return a + b;
+        }
+
+        double subtraction(double a, double b) {
+            return a - b;
+        }
+
+        double  multiplication(double a, double b) {
+            return a * b;
+        }
+
+        double division(double a, double b) {
+            return a/b;
+        }
+
+        double mod(double a, double b) {
+            return a % b;
+        }
+
+        double pow(double a, double b) {
+            return Math.pow(a, b);
+        }
+
+    }
+
